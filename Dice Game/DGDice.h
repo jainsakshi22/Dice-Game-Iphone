1fdd3360962c9bebfe02b5a66245f590deeaab42
Sakshi Jain <sakshi@Sakshis-MacBook-Pro.local>
2014-09-10 13:47:22 +0530

    added file DGDIce

---
+++ Dice Game/DGDice.h
@@ -0,0 +1,13 @@
+//
+//  DGDice.h
+//  Dice Game
+//
+//  Created by Sakshi Jain on 04/09/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DGDice : NSObject
+
+@end
---
+++ Dice Game/DGDice.m
@@ -0,0 +1,13 @@
+//
+//  DGDice.m
+//  Dice Game
+//
+//  Created by Sakshi Jain on 04/09/14.
+//
+//
+
+#import "DGDice.h"
+
+@implementation DGDice
+
+@end
